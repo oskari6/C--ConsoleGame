d32a7da8d548384532bf874a86fe1edcacad1ed8
oskari6 <oskarisulkakoski@gmail.com>
2024-11-29 13:38:04 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using NAudio.Wave;
+using ANSIConsole;
+
+namespace ConsoleApp1
+{
+    static class Program
+    {
+        private static readonly string _path = Directory.GetCurrentDirectory() + @"\test.mp3";//musiikki 
+
+        static void Main(string[] args)
+        {
+            if (!ANSIInitializer.Init(false)) ANSIInitializer.Enabled = false;
+            Console.SetOut(new Scaling(Console.Out));//skaalaus
+
+            using (var audioFile = new AudioFileReader(_path))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                    UI.GameLoop();//peli
+                }
+            }
+
+        }
+    }
+}
---
+++ Questions.cs
@@ -0,0 +1,62 @@
+﻿
+namespace ConsoleApp1
+{
+    class Questions
+    {   //kysymukset
+        private static List<string> _questions1 = new List<string>()  //tietovisan kysymykset
+        {
+            "1. Sarjassa “Doctor Who”, mikä on päähenkilön käyttämän aikakoneen nimi?\n",
+            "               2. Missä 1979 vuonna julkaistussa, Ridley Scottin elokuvassa seurataan astronautti ryhmää,\n                                 joka kohtaa tappavan alienin kaukaisella kuulla?\n",
+            "3. Minkä niminen tekoäly avustaa Tony Starkia Marver Cinematic Universessä?\n",
+            "4. Mikä on Star Warsissa nähdyn Tatooine-planeetan alamaailman valtiaan nimi?\n",
+            "5. Mikä on Mass Effect – pelisarjan päähenkilön nimi?\n",
+            "6. Kysymys: Minkä värinen Yodan valomiekka on Star Wars -universumissa?\n",
+            "7. Kuka on Matrix-trilogian ohjaaja?\n",
+        };
+        private static List<string> _questions2 = new List<string>()
+        {
+            "1. Elokuvassa “The Matrix”, mikä on elokuvan päähenkilön näyttelijän nimi?\n",
+            "2. Kuka kirjoitti scifi-novellin “Fahrenheit 451”?\n",
+            "3. Mikä oli elokuvassa “Independence Day” Maahan hyökänneiden alieneiden nimi?\n",
+            "4. Elokuvassa “The Fifth Element”, mikä on nimikkoelementin nimi?\n",
+            "5. Elokuvassa “The Matrix”, minkä värisen pillerin Neo valitsee?\n",
+            "                      6. Mikä on amerikkalaisen scifi-animaatiosarjan nimi hullun tiedemiehen\n                                    ja hänen pojanpoikansa epäonnistumisista?\n",
+            "7. Kuka ohjasi elokuvan Avatar vuodelta 2009?\n",
+        };
+        private static List<string> _questions3 = new List<string>()
+        {
+            "1. Mikä on aurinkokuntamme suurin planeetta?\n",
+            "2. Monta aurinkokuntamme planeettaa mahtuu Maan ja Kuun väliin samaan aikaan?\n",
+            "3. Missä keksittiin Astronomia?\n",
+            "4. Monta kertaa valo kiertäisi maapallon sekunnissa?\n",
+            "5. Montako kilometria valo liikkuu sekunnissa?\n",
+            "6. Mikä on kirjan ja elokuvan Ready Player One päähenkilön nimi?\n",
+            "7. Mikä on hirviön luovan tiedemiehen nimi kirjassa Frankenstein?\n",
+        };
+        private static List<string> _questions4 = new List<string>()
+        {
+            "1.Kuinka monta tuhatta on miljoonassa?\n",
+            "2. “I’m Sorry, Dave. I’m afraid I can’t do that.” Mistä elokuvasta kyseinen repliikki on?\n",
+            "3. Kuinka kauan Neptunuksen vuodenajat kestävät (Maan vuosina)?\n",
+            "4. Kuka oli 3. astronautti, joka käveli Kuun pinnalla?\n",
+            "5. Milloin meteoria kutsutaan meteoriitiksi?\n",
+            "                   6. Missä vuoden 1984 elokuvassa Arnold Schwarzenegger esittää kyborgimurhaajaa,\n                          joka lähetettiin ajassa taaksepäin tappamaan tulevan vastarintajohtajan?\n",
+            "               7. Mikä vuoden 1977 elokuva kertoo kapinallisten ryhmästä, joka taistelee pahan valtakuntaa vastaan,\n                             ja siinä on hahmoja, kuten Luke Skywalker, Han Solo ja prinsessa Leia?\n",
+        };
+        private static List<string> _questions5 = new List<string>()
+        {
+            "1. Maapallon ilmakehä vääristää auringon keltaiseksi. Minkä värinen aurinko oikeasti on?\n",
+            "2. Minkä värinen on Marsin auringonlasku?\n",
+            "3. Kuinka vanha koko Universumi on?\n",
+            "4. Elokuvassa “The District 9”, mikä on maahan jumittuneiden alieneiden nimi?\n",
+            "5. Kuka kirjoitti “The Dune”-novellin?\n",
+            "6. Missä kuuluisassa HG Wellsin scifi-romaanissa on aikakone?\n",
+            "               7. Missä vuoden 1997 elokuvassa Will Smith ja Tommy Lee Jones näyttelivät salaisia agentteja,\n                              joiden tehtävänä oli valvoa maan ulkopuolista elämää?\n",     
+        };
+        internal static List<string> Questions1 => _questions1;
+        internal static List<string> Questions2 => _questions2;
+        internal static List<string> Questions3 => _questions3;
+        internal static List<string> Questions4 => _questions4;
+        internal static List<string> Questions5 => _questions5;
+    }
+}
